49daa43e87d459a215c3fcf3c431db56758051ca
TicTac-93 <joshh123@gmail.com>
2025-02-14 10:10:29 -0500

    Removed _lua-defs folder from github, as they're purely QoL in VSCode

--- _lua-defs/export/object.lua
+++
@@ -1,4 +0,0 @@
----@meta
-
-function LoGetObjectById() end
-function LoGetWorldObjects() end
--- _lua-defs/export/sensor.lua
+++
@@ -1,8 +0,0 @@
----@meta
-
-function LoGetTWSInfo() end
-function LoGetTargetInformation() end
-function LoGetLockedTargetInformation() end
-function LoGetF15_TWS_Contacts() end
-function LoGetSightingSystemInfo() end
-function LoGetWingTargets() end
